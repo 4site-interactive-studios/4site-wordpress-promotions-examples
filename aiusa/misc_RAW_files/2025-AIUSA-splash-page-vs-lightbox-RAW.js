d2988757ae4f80dd442fcd92c2772ea33e6d987e
sydneymoyer <moyer.sydney@gmail.com>
2025-11-19 14:33:05 -0500

    New RAW Code Promo For AIUSA AB Test
    Raw code promo to run an A/B test between a full bleed splash page and a regular lightbox

---
+++ aiusa/misc_RAW_files/2025-AIUSA-splash-page-vs-lightbox-RAW.js
@@ -0,0 +1,71 @@
+<script>
+
+function checkForAdblocker() {
+  // Create an element that mimics an ad and inject it into the page
+  document.body.insertAdjacentHTML(
+    "beforeend",
+    '<ins data-adBlockTest class="adsbygoogle ad-zone ad-space ad-unit textads banner-ads banner_ads" style="display: block !important; width:1px !important; height: 1px !important; visibility: hidden !important;"></ins>'
+  );
+  const testAd = document.querySelector("[data-adBlockTest]");
+
+  // Check to see if the visitor is running an Ad Blocker
+  if (testAd) {
+    const testAdWidth = testAd.offsetWidth;
+    if (testAdWidth == "1") {
+      console.log("########################################");
+      console.log(
+        "No adblocker detected, will run Control Promotion"
+      );
+      triggerPromotions();
+    } else if (testAdWidth == "0") {
+      console.log("########################################");
+      console.log(
+        "Adblocker detected, won't run any of the Multivariate Promotions"
+      );
+      console.log(
+        "Triggering Promotion #148113: 2025 – AD BLOCKER – GT – Evergreen: Multistep Lightbox – Secure Payment Header"
+      );
+      console.log("########################################");
+      window.triggerPromotion(148113);
+    }
+  }
+}
+
+function triggerPromotions() {
+  // Define the promotions to pick from
+  const gtPromotions = ["multistep", "spinner",];
+
+  // Randomly pick and run one promotion
+  if (gtPromotions) {
+    const myPromotion =
+      gtPromotions[Math.floor(Math.random() * gtPromotions.length)];
+    if (myPromotion == "multistep") {
+      console.log(
+        "Triggering Promotion #147971: 2025 – CONTROL – GT – Evergreen: Multistep Lightbox – Secure Payment Header"
+      );
+      console.log("########################################");
+      window.triggerPromotion(147971);
+      window.dataLayer.push({
+        event: "promotion_seen",
+        promotionName:
+          "Promotion #147971: 2025 – CONTROL – GT – Evergreen: Multistep Lightbox – Secure Payment Header",
+      });
+    } else if (myPromotion == "spinner") {
+      console.log(
+       // TODO: update this name and ID number when we have it "Triggering Promotion #104047: 2024 - Multistep Lightbox - Video Asset"
+      );
+      // TODO: update this ID number when we have it window.triggerPromotion(104047);
+    }  else {
+      console.log(
+        "The promotion chosen is outside the array and nothing was triggered"
+      );
+      console.log("########################################");
+    }
+  }
+}
+
+checkForAdblocker();
+
+
+
+</script>
